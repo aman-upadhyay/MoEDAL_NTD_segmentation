12cbc1d5ea2212950b2a22486f821890300e1e87
aman <aman.upadhyay@niser.ac.in>
2020-07-06 15:37:27 +0530

    initial

---
+++ 1.py
@@ -0,0 +1,12 @@
+import cv2
+import os
+
+thresh = 75
+clean = 'Data_aman/c_250/'
+binary = 'Data_aman/b_250/'
+clean_filename = os.listdir(clean)
+for i in clean_filename:
+	image = cv2.imread(os.path.join(clean, str(i)))
+	img_binary = cv2.threshold(image, thresh, 255, cv2.THRESH_BINARY)[1]
+	m = i.replace('c', 'b')
+	cv2.imwrite(os.path.join(binary, str(m)), img_binary)
